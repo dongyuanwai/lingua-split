e7fcb3f2cba080726ffe8c193e1a95b8e76faa2d
Dong Yuanwai <yuanwaidong@gmail.com>
2023-11-23 18:06:41 +0800

    fix: css

--- src/app/sentencePractice/components/Header.tsx
+++ src/app/sentencePractice/components/Header.tsx
@@ -30,15 +30,15 @@ export default function Header() {
           <div className="flex items-center justify-center gap-2">
             <div className="relative">
               <div>
-                <button type="button" className="flex items-center justify-center rounded p-[2px] text-lg text-indigo-500 outline-none transition-colors duration-300 ease-in-out hover:bg-indigo-400 hover:text-white"
-                  title="查看错题本">
+                <button type="button" className="p-1 flex items-center justify-center rounded  text-lg text-indigo-500 outline-none transition-colors duration-300 ease-in-out hover:bg-indigo-400 hover:text-white"
+                  title="选择课程">
                   <svg viewBox="0 0 24 24" width="1.2em" height="1.2em" className="icon">
                     <path fill="currentColor" d="M6.012 18H21V4a2 2 0 0 0-2-2H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.988 5 19.805 5 19s.55-.988 1.012-1zM8 6h9v2H8V6z">
                     </path>
                   </svg>
                 </button>
               </div>
-              <div className="opacity-0 bottom-full pb-2 pointer-events-none absolute left-1/2 flex -translate-x-1/2 transform items-center justify-center transition-opacity">
+              <div className="p-1 opacity-0 bottom-full pb-2 pointer-events-none absolute left-1/2 flex -translate-x-1/2 transform items-center justify-center transition-opacity">
                 <span className="tooltip">
                   错题本
                 </span>
@@ -46,7 +46,7 @@ export default function Header() {
             </div>
             <div className="relative h-7 w-7">
               <div>
-                <button type="button" className="flex items-center justify-center rounded p-[2px] text-lg text-indigo-500 outline-none transition-colors duration-300 ease-in-out hover:bg-indigo-400 hover:text-white"
+                <button type="button" className="p-1 flex items-center justify-center rounded text-lg text-indigo-500 outline-none transition-colors duration-300 ease-in-out hover:bg-indigo-400 hover:text-white"
                   title="查看数据统计">
                   <svg viewBox="0 0 24 24" width="1.2em" height="1.2em" className="icon">
                     <g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
