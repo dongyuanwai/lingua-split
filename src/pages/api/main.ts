ea5d6cc509fe7b6f0c2164e3e94b84edb0061539
Dong Yuanwai <yuanwaidong@gmail.com>
2024-02-21 16:50:25 +0800

    feat: 新增课程

--- src/pages/api/main.ts
+++ src/pages/api/main.ts
@@ -3,11 +3,17 @@ import course01 from "@/data/01.json";
 import course02 from "@/data/02.json";
 import course03 from "@/data/03.json";
 import course04 from "@/data/04.json";
+import course05 from "@/data/05.json";
+import course06 from "@/data/06.json";
+import course07 from "@/data/07.json";
+import course08 from "@/data/08.json";
+import course09 from "@/data/09.json";
+import course10 from "@/data/10.json";
 
-const numMap = ['一','二','三','四',]
+const numMap = ['一','二','三','四','五','六','七','八','九','十']
 
 export default function GET(req: NextApiRequest, res: NextApiResponse) {
-  const courseList = [course01, course02, course03, course04].map((item,index)=>{
+  const courseList = [course01, course02, course03, course04,course06,course07,course08,course09,course10].map((item,index)=>{
     return {
       name : `第${numMap[index]}课`,
       statements: item,
